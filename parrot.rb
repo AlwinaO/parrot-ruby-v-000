601c98fc692cb0bd513bc45dce3167a171c71368
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2017-11-09 01:56:58 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot("Squawk")
-  puts "Squawk"
-  
+def parrot(phrase = "Squawk")
+  puts "Squawk!"
+
 end
 
 parrot
